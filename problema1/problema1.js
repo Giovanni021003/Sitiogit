7a10cdfc1bb2f3ff2a925ee5c1197928fef275aa
Giovanni021003 <gordillog100@gmail.com>
2020-10-13 12:48:02 -0500

    Problema1 echo

---
+++ problema1/problema1.js
@@ -0,0 +1,5 @@
+
+ 	var n1= parseInt(prompt("Dame un numero:"));
+ 	var pi = 3.14159;
+ 	var x = (4/3.0) * pi * n1*n1*n1;
+ 	document.write("&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; VOLUME = " + x.toFixed(3) + "<br>");
